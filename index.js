05a6791c8aa13eb5ab2b129bfc65d9426c49cec8
Anders Thøgersen <anders@bladre.dk>
2015-11-04 20:07:49 +0100

    Timeber templating language revived from the dead - WIP

---
+++ index.js
@@ -0,0 +1,319 @@
+// Static templates form a directory of markup
+
+var fs = require('fs'),
+    pkg = require('./package');
+
+var m = module.exports = {};
+
+var builtinDefinitions = `// Include definitions for templates v${pkg.version}
+var escapeMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        '\`': '&#x60;'
+    },
+    regex = new RegExp(Object.keys(escapeMap).join('|'), 'g');
+
+function escapeHtml(aString) {
+    return (aString + '').replace(regex, function (char) {
+        return escapeMap[char];
+    });
+}
+
+function escapeText(t) {
+   return t.replace(/(")/g, '\\\\"');
+}
+
+function escapeJson(o) {
+  return JSON.stringify(o, 0, 2);
+}
+`;
+
+// define escapeHtml and escapeText for this file
+eval(builtinDefinitions);
+
+var methods = {
+    h:   'escapeHtml',
+    t:   'escapeText',
+    j:   'escapeJson',
+    '(': 'evaluate' // TODO Implement evaluate
+};
+
+function replaceVar(args, varName) {
+  // console.warn('replaceVar', args, varName);
+  if (args[0] === '(')
+    args = '( ' + args.slice(1);
+  var a = args.split(/\s+/g);
+  if (!methods[a[0]])
+    throw new Error("No method '" + a[0] + "'");
+  var keyLookup = a.slice(1).join(' ').trim();
+  if (/^\d+$/.test(keyLookup)) {
+    keyLookup = '[' + keyLookup + ']';
+  } else if (/^\w+$/.test(keyLookup)) {
+    keyLookup = '.' + keyLookup;
+  } else if (/^\.$/.test(keyLookup)) {
+    keyLookup = '';
+  } else if (/^\W+$/.test(keyLookup)) {
+    keyLookup = '["' + keyLookup + '"]';
+  }
+  return '" + ' + methods[a[0]] + '(' + varName + keyLookup + ') + "';
+}
+
+function replaceParts(text, varName) {
+  var parts = [], pos = 0;
+  while (text && true) {
+    pos = text.search(/\{([htej(])/);
+      if (pos < 0) {
+        if (text.length > 0)
+          parts.push(escapeText(text));
+        break;
+      } else {
+        parts.push(escapeText(text.slice(0, pos)));
+        text = text.slice(pos + 1);
+      pos = text.search(/\}/);
+      if (pos < 0) {
+        throw new Error("No ending '}' found");
+      }
+      var t1 = replaceVar(text.slice(0, pos), varName);
+      parts.push(t1);
+      text = text.slice(pos + 1);
+    }
+  }
+  return '"' + parts.join('') + '";';
+}
+
+// Get nested blocks prefixed with keywords
+//
+// Task: Parse a nested text into scopes
+//
+//
+//     :loop
+//       hello
+//     end this
+//       :loop
+//         this
+//
+function parse(text) {
+  var result = [{cmd: 'function', indent: -1}],
+  scope  = result,
+  scopes = [scope],
+  re = /^([ ]*)(\S.*)$/mg,
+  m;
+
+  while ((m = re.exec(text)) !== null) {
+    var indent = m[1].length;
+
+    if (scope[0].indent >= indent) {
+      for (var i = scopes.length - 1; i >= 0; i -= 1) {
+        if (scopes[i][0].indent < indent) {
+          scope = scopes[i];
+          break;
+        }
+      }
+    }
+
+    var n = /^:(\S+)\s*(.*)$/.exec(m[2]);
+    if (n) {
+      scopes.push(scope);
+      scope.push([{cmd: n[1], args: n[2], indent: indent}]);
+      scope = scope[scope.length - 1];
+    } else {
+      if (scope.length >= 1)
+        scope.push(m[0] + '\\n');
+      else
+        scope.push(m[2]);
+      }
+  }
+  return result;
+}
+
+function spaces(n) {
+  var result = '';
+  while (n > 0) {
+    result += ' ';
+    n -= 1;
+  }
+  return result;
+}
+
+var JSspecial = {"\"": "\\\"", "\\": "\\\\", "\f": "\\f", "\b": "\\b",
+"\n": "\\n", "\t": "\\t", "\r": "\\r", "\v": "\\v"};
+function escapeJs(text) {
+  return String(text).replace(/[\"\\\f\b\n\t\r\v]/g, function(ch) {return JSspecial[ch];});
+}
+
+var varCount = -1;
+function getVarName() {
+  varCount += 1;
+  return 'v' + varCount;
+}
+
+function cmdHeader(head, indent) {
+  var indent1 = spaces(indent), indent2 = spaces(indent + 2);
+  switch(head.cmd) {
+  case 'if':
+    return ['o', indent1 + 'if (' + head.args + ') {\n' + indent2 +
+      'result += '];
+    case 'else':
+      return ['o', indent1 + 'else {\n' + indent2 + 'result += '  ];
+    case 'elsif':
+      return ['o', indent1 + 'else if (' + head.args + ') {\n' + indent2 + 'result += '];
+    case 'loop':
+      var itName = getVarName(),
+      varName = getVarName(),
+      array = head.args || 'o';
+
+      return [varName, indent1 +
+        'for (var ' + itName + ' = 0; ' +
+          itName    + ' < ' + array + '.length; ' + itName + ' += 1) {\n' +
+            indent2   + 'var ' + varName + ' = ' + 
+            array + '[' + itName + '];\n' +
+      indent2   + 'result += '];
+    case 'function':
+      return ['o', 'function (o) {\n' + indent2 + 'var result = '];
+  }
+}
+
+function stringify(ast) {
+  return _stringify(ast, 0);
+}
+
+function _stringify(ast, indent) {
+    var result   = [],
+        head     = ast[0],
+        cmd      = cmdHeader(ast[0], indent),
+        varName  = cmd[0],
+        cmdText  = cmd[1],
+        hadArray = false,
+        header   = '',
+        indent2  = spaces(indent + 2),
+        functionTextBlockCount = 0 ;
+
+    result.push(cmdText);
+    var index = 1, decls = [''], obj;
+
+    var collect = (afterArray) => {
+      if (head.cmd === 'function' || head.cmd === 'loop' || head.cmd === 'if') {
+        result[result.length - 1] += 
+        indent2 + (functionTextBlockCount === 0 ? '' : 'result += ') + 
+        replaceParts(decls.join(''), varName); 
+        functionTextBlockCount += 1;
+      } else {
+        result[result.length - 1] += replaceParts(decls.join(''), varName); 
+      }
+      decls = [''];
+    };
+
+    while (index < ast.length) {
+      obj = ast[index];
+      if ('string' === typeof obj) {
+        decls.push(obj);
+      } else {
+        if (Array.isArray(obj)) {
+          if (decls.length > 0)
+            collect(true);
+          result.push(_stringify(obj, indent + 2));
+        }
+      }
+      index += 1;
+    }
+    if (decls.length > 0)
+      collect();
+
+    switch(head.cmd) {
+    case 'if': case 'elseif': case 'else': case 'loop':
+      result.push(spaces(indent) + '}\n');
+      break;
+    case 'function':
+      result.push('return result; }\n');
+      break;
+    }
+    return result.join('\n');
+}
+
+function noext(file) {
+  return file.replace(/^([^\.]+)(?:\.\w{1,4})$/, '$1');
+}
+
+function requireFromString(src, filename) {
+  var Module = module.constructor;
+  var m = new Module();
+  m._compile(src, filename);
+  return m.exports;
+}
+
+function evalFun(fun) {
+  return new Function(builtinDefinitions + 'return ' + fun + ';')();
+}
+
+function compileDir(dirname, context, template, data) {
+  if (dirname === 'builtin')
+    return builtinDefinitions;
+
+  // TODO Handle single files too
+  var stat = fs.statSync(dirname);
+  if (!stat.isDirectory()) {
+    throw new Error("Usage: tpl <dirname>");
+  }
+
+  var blurb = '// Templates v' + pkg.version + ' compiled ' +
+                                 (new Date()).toISOString() + '\n',
+      result = [ ];
+  fs.readdirSync(dirname).forEach(function (file) {
+    if (/^\./.test(file)) return;
+    var functionName,
+    templateResult = compile(
+      fs.readFileSync(dirname + '/' + file).toString(),
+      functionName = noext(file));
+      result.push(`  ${functionName}: ${templateResult}`);
+  });
+  var isContext = context || '';
+  if (isContext === 'eval') context = 'module.exports';
+  result = context + ' = {\n' + result.join(',\n') + '\n};';
+  if (isContext === 'eval') {
+    result = builtinDefinitions + result;
+    result = requireFromString(result);
+    if (!template)            
+      return result;
+    if (!data)
+      return blurb + result[template].toString();
+    if (fs.existsSync(data))
+      data = fs.readFileSync(data).toString();
+    try {
+      data = evalFun(data);
+    } catch (e) {
+      throw new Error("Error evaluating " + data + ":" + e.message);
+    }
+    console.log('Using data' + JSON.stringify(data));
+    return result[template](data);
+  }
+  // var myself, method;
+  // if (template) {
+      // myself = requireFromString(result);
+      // // console.warn('Myself', myself);
+      // method = myself[template];
+      // if (!method) {
+          // method = function (o, i) {
+              // console.warn('Error:" No such method', template);
+          // };;
+      // }
+  // }
+  // if (myself && data) {
+      // if (!Array.isArray(data)) data = [data];
+      // return method.apply(myself, data);
+  // }
+  return blurb + result;
+}
+
+function compile(text) {
+  var ast = parse(text);
+  return stringify(ast);
+}
+
+m.replaceParts = replaceParts;
+m.parse        = parse;
+m.stringify    = stringify;
+m.compile      = compile;
+m.compileDir   = compileDir;
